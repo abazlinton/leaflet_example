df0b8be262da27ae8a37a584044ad6a13ad96be8
Alex Bazlinton <abazlinton@gmail.com>
2018-05-31 11:16:59 +0100

    CountryData naming ...

--- client/src/models/country_data.js
+++ client/src/models/country_data.js
@@ -1,11 +1,11 @@
 const Request = require('../helpers/request');
 const PubSub = require('../helpers/pub_sub');
 
-const CoutriesData = function () {
+const CountryData = function () {
   this.countries = [];
 };
 
-CoutriesData.prototype.getData = function () {
+CountryData.prototype.getData = function () {
   const request = new Request('https://restcountries.eu/rest/v2/all');
   request.get()
     .then(data => this.handleDataReady(data));
@@ -15,10 +15,10 @@ CoutriesData.prototype.getData = function () {
   });
 };
 
-CoutriesData.prototype.handleDataReady = function (countries) {
+CountryData.prototype.handleDataReady = function (countries) {
   this.countries = countries;
   PubSub.publish('CoutriesData:load', this.countries);
 };
 
 
-module.exports = CoutriesData;
+module.exports = CountryData;
